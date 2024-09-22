39f08d27ade3a3e15878516c151f8f2ac795b4c5
Danielprofessorrobotica <danielcarriello@prof.educacao.sp.gov.br>
2024-09-22 20:11:27 -0300

    Add files via upload
    projeto novo

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biblico',
+    'Quem foi Abrão?',
+    'Nome do primeiro Patriarca, que foi mudado para Abraão quando ele tinha a idade de 99 anos'
+)
+
+criaCartao(
+    'Biblico',
+    'O que é Maanaim?',
+    'Localidade em que anjos de Deus se encontram com Jacó; tornou-se cidade Levita e de Refúgio'
+)
+
+criaCartao(
+    'Biblico',
+    'O que é ação de graças',
+    'Ato de agradecimento a Deus'
+)
+
+criaCartao(
+    'Biblico',
+    'Que significa Jesus Cristo?',
+    'Deus tornando ser humano para salvar as pessoas. Jesus quer dizer Javé é Salvador; é a forma grega de Josué. Cristo quer dizer Ungido.'
+)
